5eb0f777c53b9da4133608b325128ee0de4bf235
Uladzimir Sakhonchik <33266845+zm99by@users.noreply.github.com>
2020-10-28 01:00:34 +0300

    Update jobs.groovy
    add some fix

--- jobs.groovy
+++ jobs.groovy
@@ -1,7 +1,6 @@
 us = 'vsakhonchik'
 
-
-    job("MNTLAB-$us-main-build-job") {
+job("MNTLAB-$us-main-build-job") {
         scm {
             github('MNT-Lab/dsl-task', "$us")
         }
@@ -25,47 +24,42 @@ us = 'vsakhonchik'
                     }
                     parameters {
                         predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
-                }                 
-               
+                    }
+                }
             }
-            
         }
-
-}   
-   for(i in 1..4) {
-    job("MNTLAB-$us-child${i}-build-job") {
-        scm {
-            github('MNT-Lab/dsl-task', "$us")
-        }
-        parameters {
-           activeChoiceParam('Branch') {
-          groovyScript {
-              script('''
+    for (i in 1..4) {
+        job("MNTLAB-$us-child${i}-build-job") {
+            scm {
+                github('MNT-Lab/dsl-task', "$us")
+            }
+            parameters {
+                activeChoiceParam('Branch') {
+                    groovyScript {
+                        script('''
                      def gitURL = "https://github.com/MNT-Lab/dsl-task.git"
     def command = "git ls-remote -h $gitURL"
     def proc = command.execute()
-    proc.waitFor()              
-    
-    def branches = proc.in.text.readLines().collect { 
-        it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '') 
+    proc.waitFor()
+
+    def branches = proc.in.text.readLines().collect {
+        it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
     }
     return branches ''')
-               }  
-            
-               
-       steps {
-            shell('chmod +x script.sh')
-            shell('./script.sh > output.txt')
-            shell("tar -cvf ${us}_dsl_script.tar.gz output.txt jobs.groovy script.sh") 
+                    }
+
+                    steps {
+                        shell('chmod +x script.sh')
+                        shell('./script.sh > output.txt')
+                        shell("tar -zcvf ${us}_dsl_script.tar.gz output.txt jobs.groovy script.sh")
+                    }
+                    publishers {
+                        archiveArtifacts('output.txt')
+                        archiveArtifacts('jobs.groovy')
+                        archiveArtifacts("${us}_dsl_script.tar.gz")
+                    }
+                }
+            }
         }
-       publishers { 
-			archiveArtifacts('output.txt')
-			archiveArtifacts('jobs.groovy')
-	       archiveArtifacts("${us}_dsl_script.tar.gz")
-			
-}
-   }
-} 
-   }    
-   } 
     }
+}
