da8203945e7adb031dc7ac81dbf337a222074754
Iryna Zaitsava <irinabro85@gmail.com>
2020-10-28 11:26:59 +0300

    fix : last version

--- job.groovy
+++ job.groovy
@@ -1,41 +1,8 @@
-def NAME='izaitsava'
-
-job("MNTLAB-" + NAME + "-main-build-job"){
-  parameters{
-    choiceParam('BRANCH_NAME', [NAME, 'main'], 'branch')
-    activeChoiceParam('JOBA') {
-      description('Allows user choose from multiple choices')
-      choiceType('CHECKBOX')
-        groovyScript {
-          script('''def list = []
-          (1..4).each {
-            job_list.add('MNTLAB-''' + student + '''-child' + it + '-build-job')
-          }
-          return list''')
-    }
-   }
-  }
-  steps{
-    downstreamParameterized {
-      trigger('$JOBA') {
-        block {
-          buildStepFailure('FAILURE')
-          failure('FAILURE')
-          unstable('UNSTABLE')
-        }
-        parameters {
-          predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
-        }                         
-      }
-    }
-  }
-}
-
 [1, 2, 3, 4].each {
-  job("MNTLAB-" + NAME + "-child" + it + "-build-job") {
+  job("MNTLAB-izaitsava-child${it}-build-job") {
     parameters {
       activeChoiceParam('BRANCH_NAME') {
-          choiceType('SINGLE_SELECT')
+        choiceType('SINGLE_SELECT')
           groovyScript {
             script('''def gettags = ("git ls-remote -t -h https://github.com/MNT-Lab/dsl-task.git ").execute()
                       return gettags.text.readLines().collect { 
@@ -48,14 +15,50 @@ job("MNTLAB-" + NAME + "-main-build-job"){
         github('MNT-Lab/dsl-task', '${BRANCH_NAME}')
     }
     steps{
-      shell('''chmod +x script.sh && ./script.sh > output.txt
-                tar czf ${BRANCH_NAME}_dsl_script.tar.gz *.sh *.groovy''')
+      shell('chmod +x script.sh && ./script.sh > output.txt' )
+      shell('tar czf ${BRANCH_NAME}_dsl_script.tar.gz script.sh')
     }
     publishers{
       archiveArtifacts{
-        pattern('*.txt')
+        pattern('output.txt')
         pattern('${BRANCH_NAME}_dsl_script.tar.gz')
         onlyIfSuccessful()
+      }
+    }
+  }
+} 
+
+
+job("MNTLAB-izaitsava-main-build-job"){
+  parameters{
+    choiceParam('BRANCH_NAME', ['izaitsava', 'main'])
+    activeChoiceParam('JOBA') {
+      description('Allows user choose from multiple choices')
+      choiceType('CHECKBOX')
+        groovyScript {
+          script('''
+          def list=[]
+          [1, 2, 3, 4].each {
+            list.add("MNTLAB-izaitsava-child${it}-build-job")}
+          return list
+        ''')
+    }
+   }
+  } 
+  steps{
+    downstreamParameterized {
+      trigger('$JOBA') {
+        block {
+          buildStepFailure('FAILURE')
+          failure('FAILURE')
+          unstable('UNSTABLE')
+        }
+        parameters {
+          predefinedBuildParameters {
+            properties('BRANCH_NAME=${BRANCH_NAME}')
+            textParamValueOnNewLine(false)
+          }
+        }                         
       }
     }
   }
